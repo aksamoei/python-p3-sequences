e58accb0da64362fa46d1e15c62bc25339b63695
Abraham <abukip344@gmail.com>
2024-05-22 08:37:12 -0400

    fibonachi

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        fibo = [0, 1]
+        for n in range(2, length):
+            x = fibo[-1] + fibo[-2]
+            fibo.append(x)
+        print(fibo)
+        
+
+
+print_fibonacci(1)
